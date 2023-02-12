cfac27927796ebdb736a61b2cc4bd2b1b9cbbdce
N3OB_SRABON <124287819+N3OBH4CKER@users.noreply.github.com>
2023-02-12 13:13:30 +0600

    Add files via upload

---
+++ kk.py
@@ -0,0 +1,14 @@
+# gf_dhaka.py
+def dhaka_greeting():
+    return "Hello from Dhaka!"
+
+# gf_Narayanganj.py
+def Narayanganj_greeting():
+    return "Hello from Narayanganj!"
+
+# main.py
+import gf_dhaka
+import gf_Narayanganj
+
+print(gf_dhaka.dhaka_greeting())
+print(gf_Narayanganj.Narayanganj_greeting())
